9fe464cd10ea6f38a8cdb443d4b31b40c172b7fc
Jugal Mahida <jugal.mahida.work@gmail.com>
2025-11-30 13:40:15 +0530

    Encode & Decode String

---
+++ main.py
@@ -0,0 +1,11 @@
+class Main:
+    def __init__(self) -> None:
+        # Encode & Decode String 
+        print("DSA")
+        str1 = "Hello 👋"
+        encoded_str1 = str1.encode("utf-8")
+        print(encoded_str1)
+        print(encoded_str1.decode("utf-8"))
+        
+
+Main()
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Hello, I am Jugal Mahida
+
+Today is 29/11/2025 11:29AM, i started my dsa journey with Udemy Course & MasterJI App (Indian Version of Leetcode with very specific Challenges & Problems)
+
