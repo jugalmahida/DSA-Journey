479a229b4fb1b7232a3d6b4ad986a15c05af535e
Jugal Mahida <jugal.mahida.work@gmail.com>
2025-12-04 16:59:25 +0530

    list comprehension examples

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 class Main:
     def __init__(self) -> None:
+        # Strings & Tuples are Immutable, if assign new value in same ref of string then, it will creating new 
+        # ref of it.(NOT CHANGE ORIGINAL ONES), check by id()
         # Encode & Decode String 
         print("DSA")
         str1 = "Hello 👋"
@@ -7,5 +9,21 @@ class Main:
         print(encoded_str1)
         print(encoded_str1.decode("utf-8"))
         
+    def tuples_example(self) -> None:
+        # Tuples
+        animals = ("Dog","Cat","Rat","Lion","Tiger")
+        print(animals)
+        # Destructure(unpack) Tuples
+        (a1,a2,a3,a4,a5) = animals # length of variables must be exact as length of tuple(animals   )
+        print(f"Animals - ${a1,a2,a3,a4,a5}")
+    
+    def list_comprehension_example(self) -> None:
+        # List Comprehension
+        hobby = ["Cricket","Music","Coding","New Learning"]
+        b = [x for x in hobby if "sic" in x]
+        print(b)
 
-Main()
+
+a = Main()
+# a.tuples_example()
+a.list_comprehension_example()
